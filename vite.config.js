b3354e837b864983a40b0d1d431f8f8029fd2dce
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2024-02-09 09:28:18 +0100

    foutupourfoutu

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basa:"/site-mrs-capuche/", 
-  base:"/", 
+  base:"/site-mrs-capuche/", 
+  basa:"/", 
   publicDir:"/public/",
 })
