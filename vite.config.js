09b626c4c2417e6d645a6fd1ef957dd2a80a0465
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2024-02-09 09:56:09 +0100

    oui?

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/site-mrs-capuche/", 
-  basa:"/", 
+  basa:"/site-mrs-capuche/", 
+  base:"/", 
   publicDir:"/public/",
 })
